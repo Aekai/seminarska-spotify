54b6184aec2a6a04569e443fbe67ed191aa328a9
jselic <js4941@student.uni-lj.si>
2023-05-20 16:22:22 +0200

    Created universal function for graph sampling

---
+++ Sampling.py
@@ -0,0 +1,80 @@
+import networkx as nx
+import timeit
+import json
+import random
+import pickle
+import os
+
+def sample_graph(num_files=15,**kwargs):
+    """ Return a random subsample of the spotify playlist
+    graph.
+    
+    Parameters
+    ------------
+        num_files: int, Optional
+            The number of original files out of 1.000 from which
+            a sample will be built. Default is set to 15
+        verbose: bool, Optional
+            Wheater you want the progress to be documented and the
+            total time to be printed at the end. Default is False
+        include_artists: bool, Optional
+            Whether artist nodes should be included in the sampled graph.
+            Default is True
+        include_albums: bool, Optional
+            Whether album nodes should be included in the sampled graph.
+            Default is True
+        
+    Return
+    -----------
+        Graph : networkx.Graph or networkx.DiGraph
+            The sampled graph.
+    """
+    if kwargs.get('verbose') == True:
+        timer_start = timeit.default_timer()
+       
+    trim_start = len("spotify:")
+    files = random.sample([x for x in os.listdir("./DATA/")],num_files)
+    
+    #TODO: directed switch?
+    G = nx.Graph()
+    for i,file in enumerate(files):
+        with open(f"./DATA/{file}") as read_file:
+            tmp = json.load(read_file)
+            for playlist in tmp["playlists"]:
+                #TODO: ignore newer switch?
+                G.add_node(playlist["pid"], playlist_name=playlist["name"], TYPE="PLAYLIST")
+                for track in playlist["tracks"]:
+                    track_id = track["track_uri"][trim_start:]
+                    G.add_node(track_id, track_name=track["track_name"], artist_name=track["artist_name"], TYPE="TRACK")
+                    G.add_edge(track_id, playlist["pid"])
+                    
+                    artist_id = track["artist_uri"][trim_start:]
+                    album_id = track["album_uri"][trim_start:]
+                    
+                    if kwargs.get('include_albums',True) == True:
+                       
+                        G.add_node(album_id, album_name=track["album_name"], TYPE="ALBUM")
+                        G.add_edge(album_id, track_id)
+                        if kwargs.get('include_artists',True) == True:
+                            G.add_node(artist_id, artist_name=track["artist_name"], TYPE="ARTIST")
+                            G.add_edge(artist_id, album_id)
+                            
+                    else:
+                        if kwargs.get('include_artists',True) == True:
+                            G.add_node(artist_id, artist_name=track["artist_name"], TYPE="ARTIST")
+                            G.add_edge(artist_id, track_id)
+                 
+            if kwargs.get('verbose') == True:
+                time_elapsed = timeit.default_timer() - timer_start
+                mins_elapsed = (time_elapsed)//60
+                secs_elapsed = time_elapsed % 60
+                print(f"{i+1}/{len(files)}; n:{G.number_of_nodes()}, m:{G.number_of_edges()}; time elapsed: {mins_elapsed:02.1f} min {secs_elapsed:02.1f} sec", end="\r")
+                    
+                    
+                    
+                
+    if kwargs.get('verbose') == True:
+        timer_stop = timeit.default_timer()
+        print('Time elapsed (minutes):', (timer_stop - timer_start)/60)  
+        
+    return G
